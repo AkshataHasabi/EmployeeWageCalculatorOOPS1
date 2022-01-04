761c0de4e12a9d9438bc05531a5245ff92f2dfa7
AkshataHasabi <akshuh818@gmail.com>
2022-01-04 15:15:54 +0530

    ADDED:calculated employee wage of each company

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -3,15 +3,31 @@ package com.bridgelab.empwages;
 public class EmployeeWageBuilder {
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
+	
+	private final String company; 
+	private final int empRatePerHour;
+	private final int numberOfWorkingDays;
+	private final int maximumHrsInMonth;
+	private int totalEmpWage;
+	
+	public EmployeeWageBuilder(String company, int empRatePerHour, int numberOfWorkingDays, int maximumHrsInMonth) {
+		this.company=company;
+		this.empRatePerHour=empRatePerHour;
+		this.numberOfWorkingDays=numberOfWorkingDays;
+		this.maximumHrsInMonth=maximumHrsInMonth;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("welcome to employee wage programms");
-		EmployeeWageBuilder employeeCheckObject=new EmployeeWageBuilder();
-		employeeCheckObject.computeEmpWage("DMart", 20, 20, 100);
-		employeeCheckObject.computeEmpWage("Reliance", 10, 4, 100);
+		EmployeeWageBuilder dMart=new EmployeeWageBuilder("DMart", 20, 2, 10);
+		EmployeeWageBuilder reliance=new EmployeeWageBuilder("Reliance", 20, 2, 10);
+		dMart.computeEmpWage();
+		System.out.println(dMart);
+		reliance.computeEmpWage();
+		System.out.println(reliance);
 	}
 	
-	public void computeEmpWage(String company, int empRatePerHour, int numberOfWorkingDays, int maximumHrsInMonth) {
+	public void computeEmpWage() {
 		int empHrs=0;
 		int totalWorkingDays=0;
 	    int totalEmpHrs=0;
@@ -32,7 +48,10 @@ public class EmployeeWageBuilder {
 			totalEmpHrs += empHrs;
 			System.out.println("Day: " + totalWorkingDays + "Emp Hr: " + empHrs);   	
 		}
-		int totalEmpWage = totalEmpHrs * empRatePerHour;
-		System.out.println("Total Emp Wage for a company: "+company +" "+ "is: "  + totalEmpWage);
+		totalEmpWage = totalEmpHrs * empRatePerHour;
+	}
+	@Override
+	public String toString() {
+		return "Total Emp Wage for a company: "+company +" "+ "is: "  + totalEmpWage;
 	}
 }
