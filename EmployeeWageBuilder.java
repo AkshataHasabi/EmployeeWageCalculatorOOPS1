8ad4a8886df4eec3924d9aa538d77da274772898
AkshataHasabi <akshuh818@gmail.com>
2022-01-04 13:15:33 +0530

    ADDED:checked employee part time daily wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -1,7 +1,8 @@
 package com.bridgelab.empwages;
 
 public class EmployeeWageBuilder {
-	public static final int IS_FULL_TIME=1;
+	public static final int IS_PART_TIME=1;
+	public static final int IS_FULL_TIME=2;
 	public int EMP_RATE_PER_HOUR;
 
 	public static void main(String[] args) {
@@ -14,13 +15,17 @@ public class EmployeeWageBuilder {
 		this.EMP_RATE_PER_HOUR=empRatePerHour;
 		int empHrs=0;
 		int empWage=0;
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if(empCheck==IS_FULL_TIME) {
-			empHrs = 8;
-		}else {
-			System.out.println("Employee is Absent");
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+		if (empCheck == IS_PART_TIME) {
+			empHrs = 4;
 		}
-		 empWage = empHrs * empRatePerHour;
-	     System.out.println("Emp Wage: " + empWage);
-	}
+		else if (empCheck == IS_FULL_TIME) {
+			empHrs = 8;
+		}
+		else {
+			empHrs = 0;
+		}
+		empWage = empHrs * empRatePerHour;
+		System.out.println("Emp Wage: " + empWage);   	
+		}	
 }
