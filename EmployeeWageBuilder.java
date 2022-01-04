186dabb1c4e4795a58b59758192c1cbf1b8db8e9
AkshataHasabi <akshuh818@gmail.com>
2022-01-04 12:49:22 +0530

    ADDED:checked employee daily wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -2,19 +2,25 @@ package com.bridgelab.empwages;
 
 public class EmployeeWageBuilder {
 	public static final int IS_FULL_TIME=1;
+	public int EMP_RATE_PER_HOUR;
 
 	public static void main(String[] args) {
 		System.out.println("welcome to employee wage programms");
 		EmployeeWageBuilder employeeCheckObject=new EmployeeWageBuilder();
-		employeeCheckObject.computeEmpWage();
+		employeeCheckObject.computeEmpWage(20);
 	}
 	
-	public void computeEmpWage() {
+	public void computeEmpWage(int empRatePerHour) {
+		this.EMP_RATE_PER_HOUR=empRatePerHour;
+		int empHrs=0;
+		int empWage=0;
 		double empCheck = Math.floor(Math.random() * 10) % 2;
-		if(empCheck==IS_FULL_TIME) { 
-			System.out.println("Employee is Present");
+		if(empCheck==IS_FULL_TIME) {
+			empHrs = 8;
 		}else {
 			System.out.println("Employee is Absent");
 		}
+		 empWage = empHrs * empRatePerHour;
+	     System.out.println("Emp Wage: " + empWage);
 	}
 }
