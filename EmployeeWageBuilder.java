84344090805ab1a0a24deffcb5359895bb0192b9
AkshataHasabi <akshuh818@gmail.com>
2022-01-04 13:25:27 +0530

    ADDED:checked employee part time daily wage using switch case

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -15,17 +15,18 @@ public class EmployeeWageBuilder {
 		this.EMP_RATE_PER_HOUR=empRatePerHour;
 		int empHrs=0;
 		int empWage=0;
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if (empCheck == IS_PART_TIME) {
-			empHrs = 4;
-		}
-		else if (empCheck == IS_FULL_TIME) {
-			empHrs = 8;
-		}
-		else {
-			empHrs = 0;
+		int empCheck =(int) Math.floor(Math.random() * 10) % 3;
+		switch (empCheck) {
+		case IS_PART_TIME:
+				empHrs = 4;
+				break;
+		case IS_FULL_TIME:
+        		empHrs = 8;
+        		break;
+		default:
+				empHrs = 0;
 		}
 		empWage = empHrs * empRatePerHour;
 		System.out.println("Emp Wage: " + empWage);   	
-		}	
+	}	
 }
