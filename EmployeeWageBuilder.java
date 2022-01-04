3eccec3f43b512b8ad3889390f5d263dcf55d166
AkshataHasabi <akshuh818@gmail.com>
2022-01-04 12:11:12 +0530

    ADDED:welcome message

---
+++ EmployeeWageBuilder.java
@@ -0,0 +1,8 @@
+package com.bridgelab.empwages;
+
+public class EmployeeWageBuilder {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to employee wage programms");
+	}
+}
