752b127cf96b9a4d3d002fb137cb9892be5c0223
AkshataHasabi <akshuh818@gmail.com>
2022-01-04 13:55:12 +0530

    ADDED:calculated emloyee total wage

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -4,21 +4,25 @@ public class EmployeeWageBuilder {
 	public static final int IS_PART_TIME=1;
 	public static final int IS_FULL_TIME=2;
 	public int EMP_RATE_PER_HOUR;
-	public int NUM_OF_WORKING_DAYS; 
+	public int NUM_OF_WORKING_DAYS;
+	public int MAX_HRS_IN_MONTH;
 
 	public static void main(String[] args) {
 		System.out.println("welcome to employee wage programms");
 		EmployeeWageBuilder employeeCheckObject=new EmployeeWageBuilder();
-		employeeCheckObject.computeEmpWage(20, 20);
+		employeeCheckObject.computeEmpWage(20, 20, 100);
 	}
 	
-	public void computeEmpWage(int empRatePerHour, int numberOfWorkingDays) {
+	public void computeEmpWage(int empRatePerHour, int numberOfWorkingDays, int maximumHrsInMonth) {
 		this.EMP_RATE_PER_HOUR=empRatePerHour;
 		this.NUM_OF_WORKING_DAYS=numberOfWorkingDays;
+		this.MAX_HRS_IN_MONTH=maximumHrsInMonth;
 		int empHrs=0;
-		int empWage=0;
-		int totalEmpWage=0;
-		for (int day = 0; day < numberOfWorkingDays; day++) {
+		int totalWorkingDays=0;
+	    int totalEmpHrs=0;
+	
+		while (totalEmpHrs <= maximumHrsInMonth  && totalWorkingDays < numberOfWorkingDays) {
+			totalWorkingDays++;
 			int empCheck =(int) Math.floor(Math.random() * 10) % 3;
 			switch (empCheck) {
 			case IS_PART_TIME:
@@ -30,10 +34,10 @@ public class EmployeeWageBuilder {
 			default:
 					empHrs = 0;
 			}
-			empWage = empHrs * empRatePerHour;
-			totalEmpWage += empWage;
-			System.out.println("Emp Wage: " + empWage);   	
+			totalEmpHrs += empHrs;
+			System.out.println("Day: " + totalWorkingDays + "Emp Hr: " + empHrs);   	
 		}
+		int totalEmpWage = totalEmpHrs * empRatePerHour;
 		System.out.println("Total Emp Wage: " + totalEmpWage);
 	}
 }
