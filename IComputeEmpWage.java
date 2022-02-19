196f0c381f09c9fff0ba1dc2543597f9c2b3a78d
AkshataHasabi <akshuh818@gmail.com>
2022-02-20 01:20:51 +0530

    ADDED:get total wage when queried by company

--- IComputeEmpWage.java
+++ IComputeEmpWage.java
@@ -3,4 +3,5 @@ package com.bridgelab.empwages;
 public interface IComputeEmpWage {
 	public void  addCompanyEmpWage(String company, int empRatePerHour, int numberOfWorkingDays, int maximumHrsInMonth);
 	public void computeEmpWage();
+	public int getTotalWage(String company);
 }
